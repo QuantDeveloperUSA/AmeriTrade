32dedb575aac173c85559a19b9d8df01670823c4
Anderson Fernando Domingos <anderson.ferdomingos@gmail.com>
2021-04-09 14:33:40 -0300

    API criação

---
+++ AmeriTrade.API/Authentication.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AmeriTrade.API
+{
+    public class Authentication
+    {
+        public string uri { get; set; }
+        public string host { get; set; }
+        public string port { get; set; }
+        public string clientId { get; set; }
+    }
+}
